f24049572307b638efa64b347d6720d7a472b378
blackl <blacklgames@gmail.com>
2018-05-13 23:30:09 +0300

    Improve add drug screen.

--- HealthDairy/app/src/main/java/com/blacklgames/healthdairy/AddDrugActivity.java
+++ HealthDairy/app/src/main/java/com/blacklgames/healthdairy/AddDrugActivity.java
@@ -5,9 +5,40 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class AddDrugActivity extends AppCompatActivity {
+public class AddDrugActivity extends AppCompatActivity
+{
+    public enum eDrugInputMethods
+    {
+        IM_ORAL,
+        IM_INHALATION,
+        IM_INTRAMUSCULAR,
+        IM_INTRAVENOUS,
+        IM_SUBLINGUAL,
+        IM_BUCCAL,
+        IM_RECTAL,
+        IM_VAGINAL,
+        IM_URETHRAL,
+        IM_MAX
+    }
+
+    public enum eDrugInputPeriod
+    {
+        IP_EVERY_HOUR,
+        IP_DAY,
+        IP_WEEK,
+        IP_MONTH,
+        IP_YEAR,
+        IP_MAX
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +46,62 @@ public class AddDrugActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_drug);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Добавьте Препарат");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // адаптер
+        String[] data = {"one", "two", "three", "four", "five"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spinner = (Spinner) findViewById(R.id.ad_spinInputPeriod);
+        spinner.setAdapter(adapter);
+        spinner.setPrompt("Title");
+        spinner.setSelection(0);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                // показываем позиция нажатого элемента
+                Toast.makeText(getBaseContext(), "Position = " + position, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_drug_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.ad_action_done:
+                addDrug();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
+    }
+
+    private void addDrug()
+    {
+
     }
 
 }
--- HealthDairy/app/src/main/java/com/blacklgames/healthdairy/Types.java
+++ HealthDairy/app/src/main/java/com/blacklgames/healthdairy/Types.java
@@ -2,27 +2,5 @@ package com.blacklgames.healthdairy;
 
 public class Types
 {
-    public enum eDrugInputMethods
-    {
-        IM_ORAL,
-        IM_SUBLINGUAL,
-        IM_BUCCAL,
-        IM_RECTAL,
-        IM_VAGINAL,
-        IM_URETHRAL,
-        IM_INTRAVENOUS,
-        IM_INTRAMUSCULAR,
-        IM_INHALATION,
-        IM_MAX
-    }
 
-    public enum eDrugInputPeriod
-    {
-        IP_EVERY_HOUR,
-        IP_DAY,
-        IP_WEEK,
-        IP_MONTH,
-        IP_YEAR,
-        IP_MAX
-    }
 }
